f1a8b2c6e2609459b8b8801b12116d27da0f112f
Sarn Wattanasri <mr.paxton@gmail.com>
2013-09-09 20:37:07 -0700

    initial commit

---
+++ app/views/rooms/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @room, :topic, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatty::Application.config.secret_key_base = 'a8da7506f91e7052088853db37506f02094ac60ba68a5981ea4b1df3b68bf1b6b7efd2b95ae0fa7dbb107e017874752a36377305fc85bd90b74e163add267538'
